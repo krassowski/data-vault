1c2f8846e217244b6e6a1e8cea9607649355fc8c
krassowski <5832902+krassowski@users.noreply.github.com>
2022-12-08 15:46:32 +0000

    Fix dynamic nesting, allow setting default importer

--- data_vault/dynamic_vault.py
+++ data_vault/dynamic_vault.py
@@ -14,13 +14,19 @@ class DynamicVault:
         self.cached = {}
         self.display_timestamp = display_timestamp
         self.display_metadata = display_metadata
+        self.default_importer = None
 
     def set_importers(self, importers: Dict[str, Callable]):
         self.importers = importers
+        return self
+
+    def use_importer(self, importer: Callable):
+        self.default_importer = importer
+        return self
 
     def __getattr__(self, key):
         prefixes = {
-            '/'.join(full[subset:])
+            '/'.join(full[:subset + 1])
             for item in self.__dir__()
             for full in [item.split('/')[:-1]]
             if full
@@ -30,7 +36,7 @@ class DynamicVault:
         if key in self.__dir__():
             if self.cache and key in self.cached:
                 return self.cached[key]
-            importer = self.importers.get(key, None)
+            importer = self.importers.get(key, self.default_importer)
             with record_action(
                 command=f'dynamic import of {key}',
                 verb='imported',
@@ -46,12 +52,14 @@ class DynamicVault:
             if self.cache:
                 self.cached[key] = value
             return value
-        elif path in prefixes:
-            return DynamicVault(
+        elif key in prefixes:
+            vault = DynamicVault(
                 path=path,
                 vault=self.vault,
                 cache=self.cache
             )
+            vault.importers = self.importers.copy()
+            return vault
         raise AttributeError
 
     def __dir__(self):
